b80286943c653426f132764f2c3ead6835424f53
Chris Barber <chris@cb1inc.com>
2024-12-19 01:21:54 -0600

    Dust off the cobwebs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		coverage: {
+			include: ['src/**/*.ts'],
 			reporter: ['html', 'lcov', 'text']
 		},
 		environment: 'node',
-		globals: true,
+		globals: false,
 		include: [
 			'test/**/*.test.ts'
 		],
